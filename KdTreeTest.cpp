f943ed51cdc1c277d8f3ec49e8225cf409af564c
yt-koike <y.koike.2001code@gmail.com>
2022-07-07 22:33:44 +0900

    Stable KdTree Search

--- KdTreeTest.cpp
+++ KdTreeTest.cpp
@@ -1,11 +1,37 @@
 #include "headers/kdTree.h"
 #include "headers/STL.h"
 
+Triangle* normalSearch(int triN,Triangle** tris,Vec3 p){
+    int idx = 0;
+    double minDistanceSq = triG(tris[idx]).sub(p).magSq();
+    for(int i=1;i<triN;i++){
+        Vec3 G = triG(tris[i]);
+        double distanceSq = G.sub(p).magSq();
+        if(distanceSq<minDistanceSq){
+            minDistanceSq = distanceSq;
+            idx = i;
+        }
+    }
+    return tris[idx];
+}
+
 int main()
 {
     Polygon3D sphere = STLBinLoad("ICO_Sphere.stl");
-    TreeNode* root = sphere.buildKdTree();
-    Vec3* a = search(root,Vec3(0,0,-1.0f));
-    a->print();
+    int n;
+    Triangle ** tris = sphere.getTriangles(&n);
+    std::vector<Triangle*> kdTree = makeKdTree(n,tris);
+/*
+    for(int i=0;i<kdTree.size();i++){
+      //  kdTree[i]->print();
+    }
+    */
+    Vec3 p(10);
+    std::vector<Triangle*> a = searchKdTree(&kdTree,p,80);
+    Triangle *b = normalSearch(n,tris,p);
+    for(int i=0;i<a.size();i++){
+    printf("%f\n",triG(a[i]).sub(p).mag());
+    }
+    printf("%f",triG(b).sub(p).mag());
     return 0;
 }
--- headers/kdTree.h
+++ headers/kdTree.h
@@ -6,137 +6,32 @@
 #include <algorithm>
 #include "shapes/Plane.h"
 #include "shapes/Triangle.h"
-
-typedef struct KdTreeNode
-{
-    KdTreeNode *c1, *c2;
-    Vec3 *split;
-    int isLeaf;
-    Vec3 *leaf;
-};
-
-class KdTree
-{
-private:
-    KdTreeNode *root;
-
-public:
-    void build(int N, Triangle **tris, Vec3 start, Vec3 end);
-    void build(std::vector<Triangle *> tris_vector, Vec3 start, Vec3 end, KdTreeNode *node, char d);
-    void print()
-    {
-        printRec(root);
-    }
-    void printRec(KdTreeNode *node)
-    {
-        printf("1");
-        if(node==nullptr)return;
-        if (node->isLeaf==1)
-        {
-            (node->leaf)->print();
-        }
-        else if(node->isLeaf<0){
-            printf("Error\n");
-        }else if(node->isLeaf==0)
-        {
-            printRec(node->c1);
-            printRec(node->c2);
-        }
-    }
-};
-
 Vec3 triG(Triangle *tri)
 {
     return (tri->getV1()).add(tri->getV2()).add(tri->getV3());
 }
 
-std::vector<Triangle *> sortByXYZ(std::vector<Triangle *> tris, char d)
+void sortTreeRec(std::vector<Triangle *, std::allocator<Triangle *>>::iterator begin,
+                 std::vector<Triangle *, std::allocator<Triangle *>>::iterator end,
+                 std::vector<Triangle *> *tree, char d)
 {
+    unsigned int length = std::distance(begin, end);
+    if (length <= 1)
+        return;
     switch (d)
     {
     case 'x':
-        std::sort(tris.begin(), tris.end(), [](Triangle *a, Triangle *b)
+        std::sort(begin, end, [](Triangle *a, Triangle *b)
                   { return triG(a).getX() < triG(b).getX(); });
         break;
     case 'y':
-        std::sort(tris.begin(), tris.end(), [](Triangle *a, Triangle *b)
+        std::sort(begin, end, [](Triangle *a, Triangle *b)
                   { return triG(a).getY() < triG(b).getY(); });
         break;
     case 'z':
-        std::sort(tris.begin(), tris.end(), [](Triangle *a, Triangle *b)
+        std::sort(begin, end, [](Triangle *a, Triangle *b)
                   { return triG(a).getZ() < triG(b).getZ(); });
         break;
-    default:
-        break;
-    }
-    return tris;
-}
-
-int isBetween(double a, double x, double b)
-{
-    return a <= x && x < b;
-}
-
-int isIn(Vec3 p, Vec3 start, Vec3 end)
-{
-    return isBetween(start.getX(), p.getX(), end.getX()) &&
-           isBetween(start.getY(), p.getY(), end.getY()) &&
-           isBetween(start.getZ(), p.getZ(), end.getZ());
-}
-
-/*
-int isIn(Triangle *t,Vec3 start,Vec3 end)
-{
-    return isIn(t->getV1(), start,end) || isIn(t->getV2(), start,end) || isIn(t->getV3(), start,end);
-}
-*/
-
-std::vector<Triangle *> areaFilter(std::vector<Triangle *> tris, Vec3 start, Vec3 end)
-{
-    std::vector<Triangle *> result;
-    for (int i = 0; i < tris.size(); i++)
-    {
-        if (isIn(triG(tris[i]), start, end))
-            result.push_back(tris[i]);
-    }
-    return result;
-}
-
-void KdTree::build(int N, Triangle **tris, Vec3 start, Vec3 end)
-{
-    std::vector<Triangle *> tris_vector;
-    for (int i = 0; i < N; i++)
-    {
-        Triangle *t = tris[i];
-        tris_vector.push_back(tris[i]);
-    }
-    build(tris_vector, start, end, root, 'x');
-}
-
-void KdTree::build(std::vector<Triangle *> tris_vector, Vec3 start, Vec3 end, KdTreeNode *node, char d)
-{
-    node = new KdTreeNode();
-    node->isLeaf = 0;
-    if (tris_vector.size() == 0){
-        node->isLeaf = -1;
-        return;
-    }
-    if (tris_vector.size() == 1)
-    {
-        node->isLeaf = 1;
-        node->leaf = new Vec3(triG(tris_vector[0]));
-        return;
-    }
-    tris_vector = sortByXYZ(tris_vector, d);
-    Vec3 med = triG(tris_vector[tris_vector.size() / 2]);
-    node->split = new Vec3(med);
-    if (tris_vector.size() == 2)
-    {
-        node->c1 = new KdTreeNode();
-        node->c1->leaf = new Vec3(triG(tris_vector[0]));
-        node->c2 = new KdTreeNode();
-        node->c2->leaf = new Vec3(triG(tris_vector[1]));
-        return;
     }
     if (d == 'z')
     {
@@ -146,8 +41,74 @@ void KdTree::build(std::vector<Triangle *> tris_vector, Vec3 start, Vec3 end, Kd
     {
         d++;
     }
-    build(areaFilter(tris_vector, start, med), start, med, node->c1, d);
-    build(areaFilter(tris_vector, med, end), med, end, node->c2, d);
+
+    sortTreeRec(begin, begin + length / 2, tree, d);
+    sortTreeRec(begin + length / 2 + 1 + (length % 2), end, tree, d);
+    return;
+}
+
+std::vector<Triangle *> makeKdTree(int triN, Triangle **tris)
+{
+    std::vector<Triangle *> tree;
+    for (int i = 0; i < triN; i++)
+    {
+        tree.push_back(tris[i]);
+    }
+    sortTreeRec(tree.begin(), tree.end(), &tree, 'x');
+    return tree;
+}
+void searchRec(std::vector<Triangle *, std::allocator<Triangle *>>::iterator begin,
+                    std::vector<Triangle *, std::allocator<Triangle *>>::iterator end,
+                    std::vector<Triangle *> *tree, Vec3 p, char d,int queryN, std::vector<Triangle*> *result)
+{
+    if(result->size()>=queryN)return;
+    int length = std::distance(begin, end);
+    int beginIdx = std::distance(tree->begin(), begin);
+    if (length <= 0)
+    {
+        result->push_back(tree->at(beginIdx));
+        return;
+    }
+    int middleIdx = beginIdx + length / 2 + (length % 2);
+    char way;
+    switch (d)
+    {
+    case 'x':
+        way = (p.getX() < triG(tree->at(middleIdx)).getX()) ? 'l' : 'r';
+        break;
+    case 'y':
+        way = (p.getY() < triG(tree->at(middleIdx)).getY()) ? 'l' : 'r';
+        break;
+    case 'z':
+        way = (p.getZ() < triG(tree->at(middleIdx)).getZ()) ? 'l' : 'r';
+        break;
+    }
+    if (d == 'z')
+    {
+        d = 'x';
+    }
+    else
+    {
+        d++;
+    }
+    if (way == 'l')
+    {
+        searchRec(begin, begin + length / 2, tree, p, d,queryN,result);
+        searchRec(begin + length / 2 + 1, end, tree, p, d,queryN,result);
+    }
+    else if (way == 'r')
+    {
+        searchRec(begin + length / 2 + 1, end, tree, p, d,queryN,result);
+        searchRec(begin, begin + length / 2, tree, p, d,queryN,result);
+    }
+    return;
+}
+
+std::vector<Triangle *> searchKdTree(std::vector<Triangle *> *tree, Vec3 p,int queryN)
+{
+    std::vector<Triangle *> result;
+    searchRec(tree->begin(), tree->end()-1, tree, p, 'x',queryN,&result);
+    return result;
 }
 
 #endif
